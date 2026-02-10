89be71264f4ec092429e14e01f1755130f50e024
Alex Ngo <tiendung8a6@gmail.com>
2026-02-10 15:11:40 +0700

    update code

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -18,19 +18,17 @@ export default function Home() {
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            <p
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Templates
-            </a>{" "}
+            </p>{" "}
             or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            <p
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
               Learning
-            </a>{" "}
+            </p>{" "}
             center.
           </p>
         </div>
